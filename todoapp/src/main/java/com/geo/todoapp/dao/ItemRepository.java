ded6357d025a8a902b809e0aabdcf42244bc60ad
George Mani <49112969+georgemani1225@users.noreply.github.com>
2024-12-26 18:18:52 +0530

    Update ItemRepository.java

--- todoapp/src/main/java/com/geo/todoapp/dao/ItemRepository.java
+++ todoapp/src/main/java/com/geo/todoapp/dao/ItemRepository.java
@@ -7,10 +7,6 @@ import com.geo.todoapp.entity.Item;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-    // that's it ... no need to write any code LOL!
-
-    // add a method to sort by first name(change findAll method in EmployeeServiceImpl)
     public List<Item> findAllByOrderByItemDateAsc();
 
 }
